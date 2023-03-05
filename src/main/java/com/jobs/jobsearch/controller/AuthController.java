108bc82a59e5c14bbd7af35c6d6e67306bcb089f
sachinvel <velsachin98@gmail.com>
2023-03-04 20:01:22 -0500

    change aws domain

--- src/main/java/com/jobs/jobsearch/controller/AuthController.java
+++ src/main/java/com/jobs/jobsearch/controller/AuthController.java
@@ -137,7 +137,7 @@ public class AuthController {
             String token = UUID.randomUUID().toString();
             userService.createVerificationToken(curUser, token);
             String confirmationUrl
-                    = "http://ec2-3-238-143-158.compute-1.amazonaws.com:3000/register/confirm?token=" + token;
+                    = "http://ec2-3-226-248-5.compute-1.amazonaws.com:3000/register/confirm?token=" + token;
             String message = "Click this link to verify your email : "+confirmationUrl;
             EmailSender emailSender = new EmailSender();
             emailSender.sendConfirmationLink(curUser,message);
