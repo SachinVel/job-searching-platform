8bed6e2603c5f2a24814a4b707fcdedf8f135bfb
sachinvel <velsachin98@gmail.com>
2024-02-17 15:26:58 -0500

    fix heroku deploy issue

--- src/main/java/com/jobs/jobsearch/JobsearchApplication.java
+++ src/main/java/com/jobs/jobsearch/JobsearchApplication.java
@@ -1,17 +1,11 @@
 package com.jobs.jobsearch;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 
 @SpringBootApplication
 public class JobsearchApplication {
 
-	private static final Logger LOGGER= LoggerFactory.getLogger(JobsearchApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(JobsearchApplication.class, args);
 	}
