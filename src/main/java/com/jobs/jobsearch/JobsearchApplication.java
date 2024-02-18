2b2b2702767e98251c603d239377db06f820f943
sachinvel <velsachin98@gmail.com>
2024-02-17 20:53:35 -0500

    fix heroku deploy issue

--- src/main/java/com/jobs/jobsearch/JobsearchApplication.java
+++ src/main/java/com/jobs/jobsearch/JobsearchApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobsearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobsearchApplication.class, args);
+			SpringApplication.run(JobsearchApplication.class, args);
 	}
 
 }
